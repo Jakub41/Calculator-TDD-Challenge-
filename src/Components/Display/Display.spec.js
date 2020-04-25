d806bf13fab089ce143b28e7c5637196376cb377
Jakub <lemiszewski@gmx.com>
2020-04-25 22:29:01 +0200

    Added extra displayValue test

--- src/Components/Display/Display.spec.js
+++ src/Components/Display/Display.spec.js
@@ -6,10 +6,30 @@ import { DisplayDiv } from "./styles";
 
 describe("Display", () => {
 	let wrapper;
+	const displayComponent = <Display displayValue={""} />;
 
-	beforeEach(() => (wrapper = shallow(<Display displayValue={''}/>)));
+	beforeEach(() => (wrapper = shallow(displayComponent)));
+
+	it("should render correctly", () => expect(wrapper).toMatchSnapshot());
+
+	it("Display Snapshot renderer", () => {
+		const component = renderer.create(displayComponent);
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+	it("DisplayDiv Snapshot renderer", () => {
+		const styledComponent = renderer.create(<DisplayDiv />);
+		let tree = styledComponent.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
 
 	it("should render a <DisplayDiv />", () => {
-		expect(wrapper.find(DisplayDiv)).toHaveLength;
+		expect(wrapper.find(DisplayDiv)).toHaveLength(1);
+	});
+
+	it("renders the value of displayValue", () => {
+		wrapper.setProps({ displayValue: "test" });
+		expect(wrapper.text()).toEqual("test");
 	});
 });
